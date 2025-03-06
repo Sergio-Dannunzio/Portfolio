4cc19137886bc18bfc572667bccc429bae98a503
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2025-03-06 13:28:49 -0300

    fix statsGithub

--- src/components/StatsGithub.jsx
+++ src/components/StatsGithub.jsx
@@ -3,11 +3,14 @@ import React from "react";
 const StatsGithub = () => {
   return (
     <>
-      <div className="flex flex-col items-center bg-[#17171790] p-7 rounded-2xl border border-[#262626]">
-        <img src="https://github-readme-stats.vercel.app/api?username=Sergio-Dannunzio&theme=material-palenight&show_icons=true&hide_border=true&count_private=true" alt=""
-        className="max-w-[400px] w-full" />
-        <img src="https://github-readme-streak-stats.herokuapp.com/?user=Sergio-Dannunzio&theme=material-palenight&hide_border=true" alt=""
-        className="max-w-[400px] w-full" />
+      <div className="flex flex-col bg-[#17171790] p-7 rounded-2xl border border-[#262626]">
+        <h1 className="text-2xl">GitHub Stats</h1>
+        <div className="flex flex-col items-center mt-4">
+          <img src="https://github-readme-stats.vercel.app/api?username=Sergio-Dannunzio&theme=material-palenight&show_icons=true&hide_border=true&count_private=true" alt=""
+          className="max-w-[450px] w-full" />
+          <img src="https://github-readme-streak-stats.herokuapp.com/?user=Sergio-Dannunzio&theme=material-palenight&hide_border=true" alt=""
+          className="max-w-[450px] w-full" />
+        </div>
       </div>
     </>
   );
