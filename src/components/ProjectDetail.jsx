1f483d48809392a25744eb72a69afe6ecef478f7
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2025-03-08 16:56:10 -0300

    muchos cambios a project detail

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -2,20 +2,26 @@ import React from "react";
 
 
 
-const ProjectDetail = ({ text, icon, img, color }) => {
+const ProjectDetail = ({title, text, featured, img, stacks }) => {
   return (
     <>
-      <div className="flex flex-col bg-[#000000] p-1 rounded-lg my-2 h-15 justify-center mx-2">
-            <img src={img} alt="" className="h-[354px]" />
+      <div className="flex flex-col bg-[#2020203e] p-1 rounded-lg my-2 justify-center mx-2">
+            <img src={img} alt="" className="w-full" />
             <div>
                 <div className="flex">
-                    <h1>title</h1>
-                    <p>featured</p>
+                    <h1>{title}</h1>
+                    <p>{featured}</p>
                 </div>
-                <p>text</p>
+                <div className="w-full">
+                  <p className="w-full">{text}</p>
+                </div>
                 <div className="flex">
-                    <p className="">stack</p>
-                    <p>stack</p>
+                  {stacks.map((stack, index) => (
+                    <div key={index} className={`px-1 py-0.5 ${stack.bgcolor} rounded-2xl mx-1 flex`}>
+                      <p className={` ${stack.color}`}>{stack.icon}</p>
+                      <p className={`px-1 ${stack.color} text-xs`}>{stack.text} </p>
+                    </div>
+                  ))}
                 </div>
             </div>
             <div className="flex">
--- src/pages/ProjectsDetail.jsx
+++ src/pages/ProjectsDetail.jsx
@@ -1,11 +1,60 @@
 import ProjectDetail from "../components/ProjectDetail";
+import page1 from "../assets/page1.png";
+import page3 from "../assets/page3.png";
+import page4 from "../assets/page4.png";
+import page5 from "../assets/page5.png";
+import { SiJavascript } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaNodeJs } from "react-icons/fa";
+
+const stacks33 = [
+  { text: "Angular", bgcolor: "bg-red-500", color: "text-green-100", icon: <FaNodeJs  /> },
+  { text: "Typescript", bgcolor: "bg-blue-500", color: "text-green-100", icon: <FaNodeJs  /> },
+  { text: "Sass", bgcolor: "bg-green-500", color: "text-green-100", icon: <FaNodeJs  />  },
+];
+
+const stacksTranquiliza = [
+  { text: "React", bgcolor: "bg-[#0091FF]/10", color: "text-[#0091FF]", icon: <FaReact  />   },
+  { text: "JavaScript", bgcolor: "bg-[#F7DF1E]/10", color: "text-[#F7DF1E]", icon: <SiJavascript  />  },
+  { text: "Tailwind CSS", bgcolor: "bg-[#38BDF8]/10", color: "text-[#38BDF8]", icon: <RiTailwindCssFill  />  },
+  { text: "Node.js", bgcolor: "bg-[#339933]/10", color: "text-[#339933]", icon: <FaNodeJs  />   },
+];
+
+
+const stacksBack = [
+
+];
+const stacksDeveloper = [
+
+];
 
 const ProjectsDetail = () => {
     return (
       <>
-          <div className="flex">
-              <ProjectDetail></ProjectDetail>
-              <ProjectDetail></ProjectDetail>
+          <div className="flex flex-col px-4">
+            <div className="flex w-full">
+              <ProjectDetail title={"33 Inmortals"} 
+                img={page1} 
+                text={"loremaaaaa aaaaaaaaaaa aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa aaaaaaaaaaaaaa"}
+                featured={""}
+                stacks={stacks33}></ProjectDetail>
+              <ProjectDetail title={"Bariló"}
+               img={page3}
+                text={"loremaaaaa aaaaaaaaaaaa aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaa aaaaaaaaaa"}
+                featured={""}
+                stacks={stacks33}></ProjectDetail>
+            </div>
+            <div className="flex">
+              <ProjectDetail title={"Crypto market"} img={page4}
+               text={"loremaaaaa aaaaaaaaaaaa aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaa aaaaaaaaaa"}
+               featured={""}
+               stacks={stacks33}></ProjectDetail>
+              <ProjectDetail title={"Tranquiliza 360°"} img={page5}
+               text={"loremaaaaa aaaaaaaaaaaa aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaa aaaaaaaaaa"}
+               featured={""}
+               stacks={stacksTranquiliza}></ProjectDetail>
+            </div>
           </div>
   
       </>
