64af3c77a455d3736bac07f4a21ff3e8d4c81097
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2025-03-09 19:25:03 -0300

    hover stack

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -5,14 +5,14 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 const Stack = ({ text, icon }) => {
   return (
     <>
-      <div className="flex flex-col bg-[#000000] p-1 rounded-lg my-2 h-15 justify-center">
+      <div className="group flex flex-col bg-[#000000] p-1 rounded-lg my-2 h-15 justify-center">
         <div className="flex h-full items-center">
             <div className="bg-[#1b1b1bc6] w-12 h-full mr-2 rounded-lg flex justify-center items-center text-[#8b99f8]">
-                <p className="text-3xl">{icon}</p>
+                <p className="text-3xl group-hover:text-[#8b99f8]/60">{icon}</p>
             </div>
-            <div className="flex items-center justify-between w-full">
+            <div className="flex items-center justify-between w-full ">
               <h1 className="text-md">{text}</h1>
-              <FaExternalLinkAlt className="mr-4 text-[#8b99f8]"/>
+              <FaExternalLinkAlt className="mr-4 text-[#8b99f8] group-hover:text-[#8b99f8]/60 transition"/>
             </div>
         </div>
       </div>
